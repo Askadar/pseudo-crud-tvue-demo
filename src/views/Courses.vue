34c1ca55b50c95905dc62a686f5464fe04255963
Антон Обрядин <askadar.ru@gmail.com>
2019-03-01 00:56:08 +0300

    Courses base with Controls

--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+	<div class="courses">
+		<controls page="courses" title="Courses" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+	</div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Controls from '@/components/Controls.vue'; // @ is an alias to /src
 
 @Component({
-  components: {
-    HelloWorld,
+	components: {
+		Controls,
   },
+	}
 })
-export default class Home extends Vue {}
+export default class Courses extends Vue {
+}
 </script>
