27b21b43bb6ca2c2fbd14ea2eff0f8c8f54ff5a3
Антон Обрядин <askadar.ru@gmail.com>
2019-03-10 16:34:11 +0300

    z-icon component

---
+++ src/components/ZIcon.vue
@@ -0,0 +1,50 @@
+<template>
+	<i :class="['z-icon', icon]" />
+</template>
+
+<style lang="stylus">
+.z-icon
+	font-style normal
+</style>
+
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+const icons = {
+	'edit': 'icon-feather',
+
+	'trash': 'icon-trash',
+	'delete': 'icon-trash',
+
+	'search': 'icon-search',
+
+	'add': 'icon-plus',
+
+	'arrow-left': 'icon-angle-left',
+	'arrow-right': 'icon-angle-right',
+	'arrow-up': 'icon-angle-up',
+	'arrow-down': 'icon-angle-down',
+
+	undefined: 'no_icon_provided'
+}
+
+@Component({})
+export default class ZIcon extends Vue {
+	icon = 'no_icon_provided'
+
+	created() {
+		try {
+			const d = this.$slots.default
+			// @ts-ignore cause we're in a try for a reason
+			const text = d[0].text
+			// @ts-ignore but I say it can
+			this.icon = icons[text] || text
+
+		} catch (err) {
+			this.icon = 'no_icon_provided'
+		}
+		// console.log(this)
+	}
+}
+</script>
