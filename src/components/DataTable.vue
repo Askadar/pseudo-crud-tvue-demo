2f513097cddf6064cc4d87fb848615c98d4b3763
Антон Обрядин <askadar.ru@gmail.com>
2019-03-10 16:35:10 +0300

    DataTable updates

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -4,10 +4,19 @@
 			<th v-for="column in columns" :key="column">
 				{{ column }}
 			</th>
+			<th key="actions" class="actions">
+				actions
+			</th>
 		</tr>
-		<tr v-for="(row, index) in data" :key="index">
-			<td v-for="column in row" :key="column">
-				{{ column }}
+		<tr v-for="(row, index) in data" :key="index" :class="{
+			odd: index % 2 === 1
+		}">
+			<td v-for="key in columns" :key="key">
+				{{ row[key] }}
+			</td>
+			<td key="actions" class="actions">
+				<round-button class="sub"><z-icon>edit</z-icon></round-button>
+				<round-button class="danger"><z-icon>delete</z-icon></round-button>
 			</td>
 		</tr>
 	</table>
@@ -21,27 +30,42 @@
 		padding 0.9rem 1.6rem 0.7rem
 		text-align left
 
+		&.actions
+			width 10rem
+
 	.table__heading
 		background color-main--lighten
 		color color-white
 		text-transform uppercase
 
-	td
-		padding 0.8rem 1.6rem
-		background white
+	tr
+		background #ffffff
+
+		td
+			padding 0.8rem 1.6rem
+
+			// background none
+			&.actions
+				display flex
+				justify-content space-around
+
+		&.odd
+			background #fafafa
 </style>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 
 import RoundButton from '@/components/Form/RoundButton.vue';
+import ZIcon from '@/components/ZIcon.vue';
 
 @Component({
 	components: {
 		RoundButton,
+		ZIcon,
 	}
 })
-export default class Controls extends Vue {
+export default class DataTable extends Vue {
 	@Prop({
 		type: Array,
 		default: () => (['_courses_', '_users_', 'actions'])
