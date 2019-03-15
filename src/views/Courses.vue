b43c03d4bbb1703bb1b4567a6c6e56d4791bb9c9
AZ <askadar.ru@gmail.com>
2019-03-15 12:48:37 +0300

    Courses liveining

--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -1,13 +1,22 @@
 <template>
 	<div class="courses">
-		<controls page="courses" title="Courses" />
-		<data-table :data="courses" :columns="['name', 'speaker']" />
+		<controls
+			page="courses"
+			title="Courses"
+			@search-updated="newSearch => searchFilter = newSearch"
+		/>
+		<data-table
+			:data="courses"
+			:columns="['name', 'speaker']"
+			:page-size="pageSize"
+			:filter="searchFilter"
+		/>
 	</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Controls from '@/components/Controls.vue'; // @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator'
+import Controls from '@/components/Controls.vue' // @ is an alias to /src
 import DataTable from '@/components/DataTable/DataTable.vue' // @ is an alias to /src
 
 // mock ajax api
@@ -21,6 +30,10 @@ import api from '../api'
 })
 export default class Courses extends Vue {
 	courses = []
+
+	pageSize = 5
+
+	searchFilter = ''
 
 	async mounted() {
 		const courses = (await api.get('/courses')).data
