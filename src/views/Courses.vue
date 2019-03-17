0653d56c41fb6181f1490f2bb23403b0188e0105
AZ <askadar.ru@gmail.com>
2019-03-17 20:27:31 +0300

    Courses refactor
    - plus full CRUD

--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -4,12 +4,16 @@
 			page="courses"
 			title="Courses"
 			@search-updated="newSearch => searchFilter = newSearch"
+			@page-size-updated="newPageSize => pageSize = newPageSize"
+			@course-added="courseAdded"
 		/>
 		<data-table
 			:data="courses"
 			:columns="['name', 'speaker']"
 			:page-size="pageSize"
 			:filter="searchFilter"
+			@row-edited="courseEdited"
+			@row-removed="courseRemoved"
 		/>
 	</div>
 </template>
@@ -19,6 +23,7 @@ import { Component, Vue } from 'vue-property-decorator'
 import Controls from '@/components/Controls.vue' // @ is an alias to /src
 import DataTable from '@/components/DataTable/DataTable.vue' // @ is an alias to /src
 
+import { Course } from '@/api/data'
 // mock ajax api
 import api from '../api'
 
@@ -29,15 +34,36 @@ import api from '../api'
 	}
 })
 export default class Courses extends Vue {
-	courses = []
+	courses: Array<Course> = []
 
 	pageSize = 5
 
 	searchFilter = ''
 
-	async mounted() {
+	async mounted () {
 		const courses = (await api.get('/courses')).data
 		this.courses = courses
+	}
+
+	courseAdded () {
+		this.courses.splice(0, 0, new Course({
+			name: 'New course name...', speaker: 'New course speaker...'
+		}))
+	}
+
+	async courseEdited (course: Course) {
+		const result = await api.post(`/courses/${course.id}`, course).catch(console.warn)
+		const index = this.courses.findIndex((a: Course) => a.id === course.id)
+		// this.courses[index] = result.data
+		this.courses.splice(index, 1, result.data)
+	}
+
+	async courseRemoved (courseId: string) {
+		const result = await api.delete(`/courses/${courseId}`).catch(console.warn)
+		const index = this.courses.findIndex((a: Course) => a.id === courseId)
+		// this.courses[index] = result.data
+		if (result.data.deleted)
+			this.courses.splice(index, 1)
 	}
 }
 </script>
